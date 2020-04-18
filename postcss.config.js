846da5775f86808fa6f0c2f17b33e2d5bd1de27c
SDen4 <asw325@yandex.ru>
2020-04-18 15:16:26 +0500

    delete css-mqpacker to correct work media requests

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         require("autoprefixer"),
-        require("css-mqpacker"),
         require("cssnano")({
             preset: [
                 "default", {
